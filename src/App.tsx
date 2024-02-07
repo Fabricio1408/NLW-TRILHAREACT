74f4c729abba5b484c5457953628735373d928cf
Fabricio Da Silva Ribeiro <102611358+Fabricio1408@users.noreply.github.com>
2024-02-06 23:47:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,28 @@ import React from 'react';
 
 const App: React.FC = () => {
   return (
-    <div className="mx-auto max-w-6xl my-12">
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="Nlw-expert" />
       <form className='w-full'>
         <input type="text" placeholder='Busque em suas notas...' className='bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500' />
       </form>
+      
+      <div className='h-px bg-slate-700'/>
+
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+        <div className="rounded-md bg-slate-700">
+
+        </div>
+
+        <div className="rounded-md bg-slate-800">
+
+        </div>
+
+        <div className="rounded-md bg-slate-800">
+
+        </div>
+
+      </div>
     </div>
   );
 }
